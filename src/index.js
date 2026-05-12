c6768309421eb4010c4c2b3e4e99a4b9af6cd7d4
phobia5-9 <rjdeford11@gmail.com>
2026-05-12 16:29:15 -0600

    Code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+require("dotenv").config();
 
+const {
+  Client,
+  GatewayIntentBits,
+  Collection
+} = require("discord.js");
+
+const mongoose = require("mongoose");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences
+  ]
+});
+
+client.commands = new Collection();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(console.error);
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is online`);
+});
+
+client.login(process.env.TOKEN);
